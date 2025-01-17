17a2a35073277687d64114584e5d049aa299b31c
mfauzirh <mfauzirizkih@gmail.com>
2025-01-17 10:43:07 +0700

    use config in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/mfauzirh/go-online-forum/internal/configs"
 	"github.com/mfauzirh/go-online-forum/internal/handlers/memberships"
 )
 
 func main() {
 	r := gin.Default()
 
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder([]string{"./internal/configs"}),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to initalize config: %v", err)
+	}
+
+	cfg = configs.Get()
+	log.Println("config", cfg)
+
 	membershipHandler := memberships.NewHandler(r)
 	membershipHandler.RegisterRoute()
 
-	r.Run(":8080")
+	r.Run(cfg.Service.Port)
 }
