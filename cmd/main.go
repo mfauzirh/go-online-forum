315b2795aa6affe9c60c635c0d03af221a5899a7
mfauzirh <mfauzirizkih@gmail.com>
2025-01-17 09:44:19 +0700

    try add ping pong test api

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/mfauzirh/go-online-forum/internal/handlers/memberships"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	membershipHandler := memberships.NewHandler(r)
+	membershipHandler.RegisterRoute()
+
 	r.Run(":8080")
 }
