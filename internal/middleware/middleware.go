a462a77ece15d7ea780ee3e8e08dd9f353aba429
mfauzirh <mfauzirizkih@gmail.com>
2025-01-20 14:06:10 +0700

    initialize auth middleware

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mfauzirh/go-online-forum/internal/configs"
+	"github.com/mfauzirh/go-online-forum/pkg/jwt"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	secretKey := configs.Get().Service.SecretJWT
+	return func(c *gin.Context) {
+		header := c.Request.Header.Get("Authorization")
+		header = strings.TrimSpace(header)
+		if header == "" {
+			c.AbortWithError(http.StatusUnauthorized, errors.New("missing token"))
+		}
+		userID, username, err := jwt.ValidateToken(header, secretKey)
+		if err != nil {
+			c.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+		c.Set("userID", userID)
+		c.Set("username", username)
+		c.Next()
+	}
+}
